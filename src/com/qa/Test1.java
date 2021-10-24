cdde3ab6766f91f15a3ee0945387e5cea746a51d
rohan <rohanwarang28@yahoo.com>
2021-10-24 13:12:26 +0530

    First commit

---
+++ src/com/qa/Test1.java
@@ -0,0 +1,41 @@
+package com.qa;
+
+import java.util.Arrays;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(isAnagram("listeN", "ten   lis"));
+
+	}
+	
+	
+	public static boolean isAnagram(String word1,String word2) {
+		
+		String s1 = word1.replaceAll("\\s","");
+		String s2 = word2.replaceAll("\\s","");
+		if(s1.length()!=s2.length()) {
+			
+			return false;
+		}
+		
+		else {
+			
+		char ch1[] = s1.toLowerCase().toCharArray();
+		char ch2[] = s2.toLowerCase().toCharArray();
+		
+		Arrays.sort(ch1);
+		Arrays.sort(ch2);
+		
+		//System.out.println(ch1);
+		//System.out.println(ch2);
+		return Arrays.equals(ch1, ch2);
+		
+		}
+}
+}
+
+//asian guys edit1
+//american guy edit2
+//develop branch changes
